b4fa0fe5fbc4cc854405ba64b836314ba2fc1e30
tr1x_em <admin@trix.is-a.dev>
2025-08-07 10:06:33 +0530

    Fontconfig Initialize fix

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('Gdk', '3.0')
+from gi.repository import Gtk, Gdk
+
+# Initialize fontconfig early to prevent warnings
+try:
+    import ctypes
+    libfontconfig = ctypes.CDLL("libfontconfig.so.1")
+    libfontconfig.FcInit()
+except:
+    pass
+
 from fabric.core import widgets
 import setproctitle
 from loguru import logger
