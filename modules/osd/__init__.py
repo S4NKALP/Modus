4f4636d469d1adec29e091e4d00f031b7e706bce
S4NKALP <sankalptharu50028@gmail.com>
2026-04-10 17:05:31 +0545

    chore(modules): remove controlcenter battery module

--- modules/osd/__init__.py
+++
@@ -1,8 +0,0 @@
-from .main import OSDWindow  # noqa: F401
-from .components import (  # noqa: F401
-    AudioOSDContainer,
-    BrightnessOSDContainer,
-    MicrophoneOSDContainer,
-    CapsLockOSDContainer,
-    KeyboardLayoutOSDContainer,
-)
