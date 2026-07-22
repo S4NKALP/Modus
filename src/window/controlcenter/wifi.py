af44b02a603d3a69be5189551a57fc72295edc5b
S4NKALP <sankalptharu50028@gmail.com>
2026-07-22 10:37:33 +0545

    Revert "fix(wifi): add alignment to SVG lock icon for visibility"
    This reverts commit 43c5392f1c1dc983ba682b5498fbf6c11a6101bf.

--- src/window/controlcenter/wifi.py
+++ src/window/controlcenter/wifi.py
@@ -57,11 +57,7 @@ class WifiNetworkSlot(Box):
         self.lock_icon = None
         if self.access_point.requires_password:
             self.lock_icon = svg_file(
-                "notch/lock.svg",
-                size=12,
-                name="wifi-lock-icon",
-                h_align="center",
-                v_align="center",
+                "notch/lock.svg", size=12, name="wifi-lock-icon"
             )
 
         self.password_dialog = None
